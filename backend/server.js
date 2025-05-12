b40f43b3b8c1aaaecd9b841de8c77bd2a3c781c3
RoshniSingh12220981 <roshnisinghranjan26@gmail.com>
2025-05-12 17:26:15 +0530

    auto update

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
